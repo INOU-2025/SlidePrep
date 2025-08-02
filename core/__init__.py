100db4ddacd49d66e887904a44c8729fb1fff504
Ivan Rodriguez-Conde <ivanrodriguezconde@gmail.com>
2025-08-02 15:53:42 +0200

    Minor config adjustments

--- core/__init__.py
+++ core/__init__.py
@@ -2,7 +2,7 @@ from .app_config_manager import AppConfigManager
 from .debugger import Debugger
 from .logger import Logger
 from .step import PipelineStep
-from .pipeline import Pipeline
+from core.pipeline import Pipeline
 from .container import Container
 from .bootstrap import bootstrap, get_config, get_logger, get_debugger
 
