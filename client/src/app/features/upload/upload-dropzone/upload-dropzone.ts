2d91498227fce63c2c33df6f48d149f59d1a2a29
Ivan Rodriguez-Conde <ivanrodriguezconde@gmail.com>
2026-06-12 13:02:42 +0200

    Front-end minor bugs fixed

--- client/src/app/features/upload/upload-dropzone/upload-dropzone.ts
+++ client/src/app/features/upload/upload-dropzone/upload-dropzone.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-upload-dropzone',
   imports: [],
-  templateUrl: './upload-dropzone.html',
-  styleUrl: './upload-dropzone.scss',
+  templateUrl: './upload-dropzone.component.html',
+  styleUrl: './upload-dropzone.component.scss',
 })
 export class UploadDropzone {
 
