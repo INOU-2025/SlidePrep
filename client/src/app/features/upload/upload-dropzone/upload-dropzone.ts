cd147095d2be8410652021637d7f511f7dbe09d2
Ivan Rodriguez-Conde <ivanrodriguezconde@gmail.com>
2026-06-20 00:00:22 +0200

    Dead code deleted

--- client/src/app/features/upload/upload-dropzone/upload-dropzone.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-// This component is superseded by CreateModalComponent and is no longer routed.
-@Component({
-  selector: 'app-upload-dropzone-legacy',
-  imports: [],
-  template: '',
-})
-export class UploadDropzone {}
